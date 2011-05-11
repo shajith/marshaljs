02dccb2990d9c5cbbace5f82b00c2b7a3fee0c04
Shajith Chacko <shajith@zendesk.com>
2011-05-11 00:43:14 -0700

    Simpler example script.

--- example.js
+++ example.js
@@ -1,11 +1,10 @@
 var fs = require('fs');
 var marshal = require('./marshal.js');
 
-fs.readFile(process.argv[2] || 'dump.bin', function (err, data) {
+fs.readFile(process.argv[2] || './test/dump.bin', function (err, data) {
   if (err) throw err;
-  console.log(data);
   var parser = new marshal.MarshalParser(data);
   var result = parser.parse(function(result) {
-    console.dir(result);
+    console.dir(result.object);
   });
 });
