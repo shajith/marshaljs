91be12174f4d6820770750bca3c10dc0af86160b
Shajith Chacko <shajith@zendesk.com>
2011-05-11 01:03:10 -0700

    Example script takes input on stdin as well.

--- example.js
+++ example.js
@@ -1,10 +1,22 @@
 var fs = require('fs');
 var marshal = require('./marshal.js');
 
-fs.readFile(process.argv[2] || './test/dump.bin', function (err, data) {
-  if (err) throw err;
-  var parser = new marshal.MarshalParser(data);
-  var result = parser.parse(function(result) {
-    console.dir(result.error || result.object);
+function parseData(data) {
+    var parser = new marshal.MarshalParser(data);
+    var result = parser.parse(function(result) {
+      console.dir(result.error || result.object);
+    });
+}
+
+if(process.argv[2]) {
+  fs.readFile(process.argv[2], function (err, data) {
+    if (err) throw err;
+    parseData(data);
   });
-});
+} else {
+  var stdin = process.openStdin();
+  stdin.setEncoding('utf8');
+  stdin.on('data', function (chunk) {
+    parseData(new Buffer(chunk));
+  });
+}
